cfb3046667e812e870ff819a47aee0730ab55329
Ana Caroline Rodrigues Neves <97240075+Ana-Neves@users.noreply.github.com>
2023-02-12 15:09:27 -0300

    Add files via upload

---
+++ ifeelse.dart
@@ -0,0 +1,9 @@
+void main() {
+  int A = 5, B = 6, C = 7, D = 8;
+
+  if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0) {
+    print("Valores aceitos");
+  } else {
+    print("Valores não aceitos");
+  }
+}
